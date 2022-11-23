26f8dce9e0881c04926d25f8966ee41804281545
Jordi Font <jordifv90@gmail.com>
2022-11-23 13:06:59 +0100

    Add util to decode token

---
+++ src/utils/decodetoken.ts
@@ -0,0 +1,13 @@
+import jwtDecode from "jwt-decode";
+import { JwtPayloadCustom } from "../types/types";
+
+const decodeToken = (token: string) => {
+  const jwtPayload: JwtPayloadCustom = jwtDecode(token);
+
+  return {
+    id: jwtPayload.id,
+    username: jwtPayload.username,
+  };
+};
+
+export default decodeToken;
