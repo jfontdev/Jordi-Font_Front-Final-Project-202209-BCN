84bb296a90cfdd38ec220767edfa07d8163faa34
Jordi Font <jordifv90@gmail.com>
2022-11-24 18:00:17 +0100

    Add mainTheme and Globalstyles to app component, remove index.css

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
-import "./index.css";
+import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./styles/mainTheme";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -11,7 +13,10 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={mainTheme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
