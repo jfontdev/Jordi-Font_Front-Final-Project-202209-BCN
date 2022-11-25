448247ae4fef15f98c897c521af41a78bffc5aac
Jordi Font <jordifv90@gmail.com>
2022-11-25 11:51:57 +0100

    Add DefaultTheme properties from themeStyles.

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from "styled-components";
+import "@fontsource/poppins";
 
 const GlobalStyles = createGlobalStyle`
   *,
--- src/styles/mainTheme.ts
+++ src/styles/mainTheme.ts
@@ -1,7 +1,49 @@
-import { themeSizes } from "./themesStyles";
+import { DefaultTheme } from "styled-components";
 
-const mainTheme = {
-  paddingHorizontal: themeSizes.body.paddingHorizontal,
+const mainTheme: DefaultTheme = {
+  colors: {
+    header: {
+      light: "#0795e6",
+      dark: "#003452",
+    },
+    button: {
+      dark: "#12194a",
+      medium: "#387ea5",
+      light: "#0077da",
+    },
+    review: {
+      dark: "#003452",
+      light: "#0795E6",
+      user: "#136cb2",
+    },
+    footer: {
+      light: "#909090",
+    },
+    letter: {
+      dark: "#000000",
+      light: "#f7F7fc",
+    },
+    modalSuccess: {
+      accent: "#00BA88",
+      background: "#f2fffB",
+    },
+    modalError: {
+      accent: "#c30052",
+      background: "#ed2e7e",
+    },
+  },
+  sizes: {
+    button: {
+      padding: "1rem 1rem",
+      borderRadius: "40px",
+    },
+    body: {
+      paddingHorizontal: "20px",
+    },
+  },
+  font: {
+    mainFont: "Poppins",
+  },
 };
 
 export default mainTheme;
