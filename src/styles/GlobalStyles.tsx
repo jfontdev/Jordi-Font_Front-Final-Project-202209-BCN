36f53fbda6cf6165ad8ad11c0aa36a4bdfc2fb31
jfontvit <jordifv90@gmail.com>
2022-11-27 13:07:22 +0100

    Use modern-normalize reset, add more globalStyles

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,52 +1,63 @@
+import "modern-normalize";
 import { createGlobalStyle } from "styled-components";
 import "@fontsource/poppins";
+import mainTheme from "./mainTheme";
 
 const GlobalStyles = createGlobalStyle`
-  *,
-*::before,
-*::after {
-  box-sizing: border-box;
-}
 
-html,
 body {
-  height : 100%;
-  margin : 0;
+line-height: 1.5;
+font-family: ${mainTheme.font.mainFont};
 }
 
-html {
-  font-family: "Poppins", Helvetica, Arial, Verdana, Tahoma, sans-serif;
+.container {
+  width: 100%;
+  padding-right: calc(${mainTheme.grid.paddingX} * 0.5);
+  padding-left: calc(${mainTheme.grid.paddingX} * 0.5);
+  margin-right: auto;
+  margin-left: auto;
 }
 
-ul {
-  list-style: none;
-
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: calc(-0.5 * ${mainTheme.grid.paddingX});
+  margin-left: calc(-0.5 * ${mainTheme.grid.paddingX});
+  & > * {
+    flex-shrink: 0;
+    width: 100%;
+    max-width: 100%;
+    padding-right: calc(${mainTheme.grid.paddingX} * 0.5);
+    padding-left: calc(${mainTheme.grid.paddingX} * 0.5);
+    }
 }
 
-input,
-button,
-textarea,
-select {
-  font :inherit
+.col {
+  flex: 1 0 0%;
 }
 
-p,
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  overflow-wrap :break-word;
-  margin: 0
-}
+.col-6 {
+  flex: 0 0 auto;
+  width: 25%;
+ }
 
-a {
-  text-decoration: none;
-  color: inherit;
-  cursor: pointer;
+@media (min-width: ${mainTheme.screens.xl}) {
+  .container {
+    max-width: ${mainTheme.grid.container.xl}
+  }
+  .col-xl-3 {
+    flex: 0 0 auto;
+    width: 25%;
+  }
+  .col-xl-4 {
+    flex: 0 0 auto;
+    width: 33.33333333%;
+  }
+  .col-xl-8 {
+    flex: 0 0 auto;
+    width: 66.66666667%;
+  }
 }
-
 `;
 
 export default GlobalStyles;
