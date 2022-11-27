2a07478325cb99adfd21a35f81803231d7792aba
jfontvit <jordifv90@gmail.com>
2022-11-27 13:05:04 +0100

    Add render with providers mock

---
+++ src/mocks/renderWithProviders.tsx
@@ -0,0 +1,42 @@
+import { configureStore, PreloadedState } from "@reduxjs/toolkit";
+import { render, RenderOptions } from "@testing-library/react";
+import React, { PropsWithChildren } from "react";
+import { Provider } from "react-redux";
+import { DefaultTheme, ThemeProvider } from "styled-components";
+import { uiReducer } from "../redux/features/uiSlice/uiSlice";
+import { userReducer } from "../redux/features/usersSlice/usersSlice";
+import { RootState, store } from "../redux/store";
+import GlobalStyles from "../styles/GlobalStyles";
+import mainTheme from "../styles/mainTheme";
+
+interface ExtendedRenderOptions extends RenderOptions {
+  preloadedState?: PreloadedState<RootState>;
+  store?: typeof store;
+  theme?: DefaultTheme;
+}
+
+const renderWithProviders = (
+  ui: React.ReactElement,
+  {
+    preloadedState,
+    store = configureStore({
+      reducer: { ui: uiReducer, user: userReducer },
+      preloadedState,
+    }),
+    ...renderOptions
+  }: ExtendedRenderOptions = {}
+) => {
+  const Wrapper = ({ children }: PropsWithChildren<{}>): JSX.Element => {
+    return (
+      <Provider store={store}>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyles />
+          {children}
+        </ThemeProvider>
+      </Provider>
+    );
+  };
+  return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
+};
+
+export default renderWithProviders;
