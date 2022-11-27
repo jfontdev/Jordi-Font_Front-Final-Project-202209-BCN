434288d988560b60e82f60e9ab67e9ace8bbc805
jfontvit <jordifv90@gmail.com>
2022-11-27 18:31:45 +0100

    Add classname and type props to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,23 @@
+import ButtonStyled from "./ButtonStyled";
+
 interface ButtonProps {
   text: string;
   action?: () => void;
+  className?: string;
+  type: "submit" | "button" | "reset";
 }
 
-const Button = ({ text, action }: ButtonProps): JSX.Element => {
-  return <button onClick={action}>{text}</button>;
+const Button = ({
+  text,
+  action,
+  className,
+  type,
+}: ButtonProps): JSX.Element => {
+  return (
+    <ButtonStyled onClick={action} className={className} type={type}>
+      {text}
+    </ButtonStyled>
+  );
 };
 
 export default Button;
