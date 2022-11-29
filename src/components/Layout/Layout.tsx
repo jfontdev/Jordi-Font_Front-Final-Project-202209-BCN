e6e8a28ac60d86f81bf9b42a030e62325a52537b
jfontvit <jordifv90@gmail.com>
2022-11-29 19:58:22 +0100

    Add Modal conditional rendering in Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { useAppSelector } from "../../redux/hooks";
 import Header from "../Header/Header";
+import Modal from "../Modal/Modal";
 import LayoutStyled from "./LayoutStyled";
 
 const Layout = (): JSX.Element => {
+  const { isOpen } = useAppSelector(({ ui }) => ui);
+
   return (
     <LayoutStyled>
+      {isOpen && <Modal />}
       <Header />
       <Outlet></Outlet>
     </LayoutStyled>
