5baf646afc555eff6f23b8aefa876e83ca68fcd5
jfontvit <jordifv90@gmail.com>
2022-11-29 11:48:07 +0100

    Add HomePage page and initial styles

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "../components/LoginForm/LoginForm";
+import HomePageStyled from "./HomePageStyled";
+
+const HomePage = (): JSX.Element => {
+  return (
+    <HomePageStyled>
+      <h1>Login</h1>
+      <LoginForm />
+    </HomePageStyled>
+  );
+};
+
+export default HomePage;
