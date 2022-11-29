831fc69370576cfd091949506cf84d5087d5f7a8
jfontvit <jordifv90@gmail.com>
2022-11-29 12:46:03 +0100

    Add Register page route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import HomePage from "./pages/Homepage/HomePage";
+import Register from "./pages/Register/Register";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
+      <Route path="/register" element={<Register />}></Route>
     </Routes>
   );
 };
