f07d95d9f7d12da615eb0e4c2e7b7740513096fb
jfontvit <jordifv90@gmail.com>
2022-11-29 12:43:18 +0100

    Change gap, width and margin of LoginForm component

--- src/components/LoginForm/LoginFormStyled.tsx
+++ src/components/LoginForm/LoginFormStyled.tsx
@@ -6,8 +6,8 @@ const LoginFormStyled = styled.form`
   flex-direction: column;
   align-items: center;
   margin-top: ${mainTheme.pxToRem(10)};
-  gap: 10px;
-  width: 50%;
+  gap: 8px;
+  width: 70%;
   .form_label {
     font-size: ${mainTheme.pxToRem(20)};
   }
@@ -15,7 +15,7 @@ const LoginFormStyled = styled.form`
     font-size: 20px;
   }
   .span-question {
-    margin-top: ${mainTheme.pxToRem(40)};
+    margin-top: ${mainTheme.pxToRem(24)};
   }
   .span-link {
     margin-top: ${mainTheme.pxToRem(10)};
