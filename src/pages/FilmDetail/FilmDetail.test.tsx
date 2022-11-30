9cd288c56116f8016956e91d489a358d6dcc762b
Jordi Font <jordifv90@gmail.com>
2022-11-30 13:12:51 +0100

    Add FilmDetail test

---
+++ src/pages/FilmDetail/FilmDetail.test.tsx
@@ -0,0 +1,23 @@
+import { screen } from "@testing-library/react";
+import renderWithProviders from "../../mocks/renderWithProviders";
+import FilmDetail from "./FilmDetail";
+
+describe("Given a FilmDetail component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should display an image with alt text 'Pirates of the caribbean movie poster' and a movie title 'Piratas del Caribe'", () => {
+      const expectedMovieTitle = "Piratas del Caribe";
+      const expectedMoviePoster = "Pirates of the caribbean movie poster";
+
+      renderWithProviders(<FilmDetail />);
+
+      const movieTitle = screen.getByRole("heading", {
+        level: 1,
+        name: expectedMovieTitle,
+      });
+      const moviePoster = screen.getByAltText(expectedMoviePoster);
+
+      expect(movieTitle).toBeInTheDocument();
+      expect(moviePoster).toBeInTheDocument();
+    });
+  });
+});
