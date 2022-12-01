20aeb8c27d1bd56120c57a37c5dcd611de926adc
Jordi Font <jordifv90@gmail.com>
2022-12-01 17:01:09 +0100

    Refactor styles of MovieDetail

--- src/components/MovieDetail/MovieDetail.tsx
+++ src/components/MovieDetail/MovieDetail.tsx
@@ -3,13 +3,16 @@ import MovieDetailStyled from "./MovieDetailStyled";
 const MovieDetail = (): JSX.Element => {
   return (
     <MovieDetailStyled>
-      <img
-        src="../../../images/Piratas Del Caribe La Maldicion De La Perla Negra.webp"
-        alt="Pirates of the caribbean movie poster"
-        height="518"
-        width="350"
-        className="movie-poster"
-      />
+      <div className="movie-poster-container">
+        <img
+          src="../../../images/Piratas Del Caribe La Maldicion De La Perla Negra.webp"
+          alt="Pirates of the caribbean movie poster"
+          height="518"
+          width="350"
+          className="movie-poster"
+        />
+      </div>
+
       <div className="movie-details">
         <h1 className="movie-title">Piratas del Caribe</h1>
         <div className="movie-year-container">
