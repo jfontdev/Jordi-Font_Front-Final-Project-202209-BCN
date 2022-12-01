322a6f51e3c8337a76347264564d2329a5f5f29b
Jordi Font <jordifv90@gmail.com>
2022-12-01 13:19:15 +0100

    Add mocks for useReview tests

--- src/mocks/review/mockReview.ts
+++ src/mocks/review/mockReview.ts
@@ -1,7 +1,29 @@
 import { ReviewsState } from "../../redux/features/reviewsSlice/types";
 
-const mockEmptyReview: ReviewsState = {
+export const mockEmptyReview: ReviewsState = {
   reviewsList: [],
 };
 
-export default mockEmptyReview;
+export const mockReviews = [
+  {
+    _id: "6385fda659fac43576020b2c",
+    title: "abc",
+    reviewText: "abc",
+    rating: 10,
+    favoriteScene: "abc.jpg",
+  },
+  {
+    _id: "6385fda659fac43576020c3d",
+    title: "def",
+    reviewText: "def",
+    rating: 10,
+    favoriteScene: "def",
+  },
+  {
+    _id: "6385fda659fac43576020f2d",
+    title: "def",
+    reviewText: "def",
+    rating: 10,
+    favoriteScene: "def",
+  },
+];
---
+++ src/mocks/review/mockReviewList.ts
@@ -0,0 +1,25 @@
+export const mockReviewList = {
+  reviewsList: [
+    {
+      _id: "6385fda659fac43576020b2c",
+      title: "abc",
+      reviewText: "abc",
+      rating: 10,
+      favoriteScene: "abc.jpg",
+    },
+    {
+      _id: "6385fda659fac43576020c3d",
+      title: "def",
+      reviewText: "def",
+      rating: 10,
+      favoriteScene: "def",
+    },
+    {
+      _id: "6385fda659fac43576020f2d",
+      title: "def",
+      reviewText: "def",
+      rating: 10,
+      favoriteScene: "def",
+    },
+  ],
+};
