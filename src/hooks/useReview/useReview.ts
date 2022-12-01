6cc90f437cbc3a07df80ac2a1e25029f853f63fe
Jordi Font <jordifv90@gmail.com>
2022-12-01 13:21:44 +0100

    Add useReview custom hook with loadReviewsList

---
+++ src/hooks/useReview/useReview.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useCallback } from "react";
+import { loadReviewsActionCreator } from "../../redux/features/reviewsSlice/reviewsSlice";
+import { openModalActionCreator } from "../../redux/features/uiSlice/uiSlice";
+import { useAppDispatch } from "../../redux/hooks";
+
+const useReview = () => {
+  const dispatch = useAppDispatch();
+  const url = process.env.REACT_APP_API_URL;
+
+  const loadReviewsList = useCallback(async () => {
+    try {
+      const response = await axios.get(`${url}reviews`);
+
+      const responseData = response.data;
+
+      const { reviewsList } = responseData;
+
+      dispatch(loadReviewsActionCreator(reviewsList));
+    } catch (error: unknown) {
+      dispatch(
+        openModalActionCreator({
+          isError: true,
+          message: "Esta película aún no tiene reseñas.",
+        })
+      );
+    }
+  }, [dispatch, url]);
+
+  return { loadReviewsList };
+};
+
+export default useReview;
