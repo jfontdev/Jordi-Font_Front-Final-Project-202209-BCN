5617ad7cdb1079ee196aecef978ca3a9f6c149d4
Jordi Font <jordifv90@gmail.com>
2022-12-02 10:12:25 +0100

    Add ReviewListCard component to FilmDetail page

--- src/pages/FilmDetail/FilmDetail.tsx
+++ src/pages/FilmDetail/FilmDetail.tsx
@@ -1,10 +1,12 @@
 import MovieDetail from "../../components/MovieDetail/MovieDetail";
+import ReviewCardList from "../../components/ReviewCardList/ReviewCardList";
 import FilmDetailStyled from "./FilmDetailStyled";
 
 const FilmDetail = (): JSX.Element => {
   return (
     <FilmDetailStyled>
       <MovieDetail />
+      <ReviewCardList />
     </FilmDetailStyled>
   );
 };
