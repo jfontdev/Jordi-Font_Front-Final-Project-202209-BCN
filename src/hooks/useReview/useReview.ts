af949ccdd554aa866a0da8c73410871f2c826db1
jfontvit <jordifv90@gmail.com>
2022-12-04 14:19:00 +0100

    Add createReview custom hook

--- src/hooks/useReview/useReview.ts
+++ src/hooks/useReview/useReview.ts
@@ -1,15 +1,19 @@
 import axios from "axios";
 import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+
 import {
   deleteReviewActionCreator,
   loadReviewsActionCreator,
 } from "../../redux/features/reviewsSlice/reviewsSlice";
+import { Review } from "../../redux/features/reviewsSlice/types";
 import { openModalActionCreator } from "../../redux/features/uiSlice/uiSlice";
 import { useAppDispatch } from "../../redux/hooks";
 
 const useReview = () => {
   const dispatch = useAppDispatch();
   const url = process.env.REACT_APP_API_URL;
+  const navigate = useNavigate();
 
   const loadReviewsList = useCallback(async () => {
     try {
@@ -50,7 +54,27 @@ const useReview = () => {
     }
   };
 
-  return { loadReviewsList, deleteReview };
+  const createReview = async (reviewData: Review) => {
+    try {
+      await axios.post(`${url}reviews/create`, reviewData);
+      dispatch(
+        openModalActionCreator({
+          isError: false,
+          message: "Reseña creada",
+        })
+      );
+      navigate("/film-detail");
+    } catch (error: unknown) {
+      dispatch(
+        openModalActionCreator({
+          isError: true,
+          message: "No puedes crear reseñas ahora mismo.",
+        })
+      );
+    }
+  };
+
+  return { loadReviewsList, deleteReview, createReview };
 };
 
 export default useReview;
