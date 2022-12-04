412f3b030cdf96eeb7826c56506e5f56ad66f65b
jfontvit <jordifv90@gmail.com>
2022-12-04 14:20:31 +0100

    Fix FilmDetail test by adding react-router-dom mock

--- src/pages/FilmDetail/FilmDetail.test.tsx
+++ src/pages/FilmDetail/FilmDetail.test.tsx
@@ -2,6 +2,11 @@ import { screen } from "@testing-library/react";
 import renderWithProviders from "../../mocks/renderWithProviders";
 import FilmDetail from "./FilmDetail";
 
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => jest.fn(),
+}));
+
 describe("Given a FilmDetail component", () => {
   describe("When it is rendered", () => {
     test("Then it should display an image with alt text 'Pirates of the caribbean movie poster' and a movie title 'Piratas del Caribe'", () => {
