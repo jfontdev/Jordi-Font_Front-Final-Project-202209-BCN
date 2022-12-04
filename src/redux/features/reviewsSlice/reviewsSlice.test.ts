c82325a9666ffaf535b1ed739d4c09a369800ecb
jfontvit <jordifv90@gmail.com>
2022-12-04 13:25:25 +0100

    Add test for createReview reducer

--- src/redux/features/reviewsSlice/reviewsSlice.test.ts
+++ src/redux/features/reviewsSlice/reviewsSlice.test.ts
@@ -1,9 +1,11 @@
 import { mockReviewToDelete } from "../../../mocks/review/mockReview";
 import { mockReviewList } from "../../../mocks/review/mockReviewList";
 import {
+  createReviewActionCreator,
   deleteReviewActionCreator,
   loadReviewsActionCreator,
   reviewsReducer,
+  initialState,
 } from "./reviewsSlice";
 import { Reviews, ReviewsState } from "./types";
 
@@ -46,6 +48,28 @@ describe("Given the reviewsReducer", () => {
       const newState = reviewsReducer(mockReviewList, action);
 
       expect(newState).toStrictEqual(expectedState);
+    });
+  });
+
+  describe("When it is invoked with the reducer createReview", () => {
+    test("Then it should recieve a list of reviews with the new review created", () => {
+      const newReview = {
+        _id: "6385fda659fac43576020b2c",
+        title: "abc",
+        reviewText: "abc",
+        rating: 10,
+        favoriteScene: "abc.jpg",
+      };
+
+      const action = createReviewActionCreator(newReview);
+      const expectedNewReviewList = {
+        ...initialState,
+        reviewList: [...initialState.reviewList, newReview],
+      };
+
+      const newState = reviewsReducer(initialState, action);
+
+      expect(newState).toStrictEqual(expectedNewReviewList);
     });
   });
 });
