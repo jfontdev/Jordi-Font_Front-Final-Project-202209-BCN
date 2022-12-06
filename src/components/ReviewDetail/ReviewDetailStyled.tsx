3b61ba4a798a7651b5d01fe0542d3b88989631e6
Jordi Font <jordifv90@gmail.com>
2022-12-06 09:33:55 +0100

    Add ReviewDetail component and its initial styles

---
+++ src/components/ReviewDetail/ReviewDetailStyled.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+import mainTheme from "../../styles/mainTheme";
+
+const ReviewDetailStyled = styled.article`
+  border: 2px solid ${mainTheme.colors.review.dark};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 90%;
+  margin-top: ${mainTheme.pxToRem(33)};
+  margin-left: ${mainTheme.pxToRem(20)};
+  margin-right: ${mainTheme.pxToRem(20)};
+  .review-container {
+    margin-left: ${mainTheme.pxToRem(10)};
+  }
+  .review-text {
+    margin-right: ${mainTheme.pxToRem(10)};
+  }
+  .review-scene-container {
+    margin-top: ${mainTheme.pxToRem(10)};
+    margin-right: ${mainTheme.pxToRem(10)};
+    overflow: hidden;
+  }
+  .review-scene {
+    margin-top: ${mainTheme.pxToRem(10)};
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  .review-rating {
+    margin-bottom: ${mainTheme.pxToRem(16)};
+  }
+`;
+
+export default ReviewDetailStyled;
