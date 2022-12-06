7cdb4a2b219e2379c5e31210f0a00d979710015d
Jordi Font <jordifv90@gmail.com>
2022-12-06 09:34:06 +0100

    Add test for ReviewDetail component

---
+++ src/components/ReviewDetail/ReviewDetail.test.tsx
@@ -0,0 +1,34 @@
+import { screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import renderWithProviders from "../../mocks/renderWithProviders";
+import ReviewDetail from "./ReviewDetail";
+
+describe("Given a ReviewDetail component", () => {
+  describe("When it is rendered", () => {
+    test("Then it should display a review with an image that have an attribute src and a review title 'Maravillosa'", () => {
+      const expectedReviewTitle = "Maravillosa";
+      const expectedReviewScene =
+        "../../../images/Piratas Del Caribe La Maldicion De La Perla Negra.webp";
+
+      renderWithProviders(
+        <BrowserRouter>
+          <ReviewDetail
+            title={expectedReviewTitle}
+            review="Siempre la recordaréis como la película que convirtió a Johnny Depp en una superestrella y casi le hace ganar un Oscar. El hecho de que no ganara no es un problema, ya que gracias a él esta es la mejor película de piratas jamás realizada."
+            rating={10}
+            favoriteScene={expectedReviewScene}
+          />
+        </BrowserRouter>
+      );
+
+      const reviewTitle = screen.getByRole("heading", {
+        level: 2,
+        name: expectedReviewTitle,
+      });
+      const reviewScene = screen.getByRole("img");
+
+      expect(reviewTitle).toBeInTheDocument();
+      expect(reviewScene).toHaveAttribute("src", expectedReviewScene);
+    });
+  });
+});
