5fc8850326cdaecabbc0e8ddc2ca71ab29aa946b
Jordi Font <jordifv90@gmail.com>
2022-12-07 09:28:05 +0100

    Add lazy-loading for FilmDetail and Detail pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,30 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import CreateReview from "../../pages/CreateReview/CreateReview";
-import FilmDetail from "../../pages/FilmDetail/FilmDetail";
 import HomePage from "../../pages/Homepage/HomePage";
 import Register from "../../pages/Register/Register";
-import Detail from "../../pages/Detail/Detail";
+import React, { Suspense } from "react";
+import Loading from "../Loading/Loading";
+
+const FilmDetail = React.lazy(
+  () => import("../../pages/FilmDetail/FilmDetail")
+);
+
+const Detail = React.lazy(() => import("../../pages/Detail/Detail"));
 
 const App = () => {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/register" element={<Register />}></Route>
-        <Route path="/film-detail" element={<FilmDetail />}></Route>
-        <Route path="/create-review" element={<CreateReview />}></Route>
-        <Route path="/review-detail/:id" element={<Detail />}></Route>
-      </Route>
-    </Routes>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/film-detail" element={<FilmDetail />}></Route>
+          <Route path="/create-review" element={<CreateReview />}></Route>
+          <Route path="/review-detail/:id" element={<Detail />}></Route>
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
