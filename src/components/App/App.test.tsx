ff3ca047be24041fcf2c46c73e4ddf75f46d8649
Jordi Font <jordifv90@gmail.com>
2022-12-07 10:37:26 +0100

    Add tests for lazy-loading routes

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,6 +1,8 @@
 import TestRenderer from "react-test-renderer";
 import { MemoryRouter } from "react-router-dom";
 import App from "./App";
+import renderWithProviders from "../../mocks/renderWithProviders";
+import { screen, waitFor } from "@testing-library/react";
 
 describe("Given an App component", () => {
   describe("When its rendered", () => {
@@ -11,6 +13,52 @@ describe("Given an App component", () => {
         </MemoryRouter>
       );
       expect(expectedApp).toMatchSnapshot();
+    });
+  });
+});
+
+describe("When it's rendered with path '/film-detail'", () => {
+  test("Then it should show the film detail page", async () => {
+    const filmDetailPath = "/film-detail";
+    const expectedHeadingText = "MovieRating";
+    const expectedHeadingLevel = 2;
+
+    renderWithProviders(
+      <MemoryRouter initialEntries={[filmDetailPath]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      const expectedHeading = screen.queryByRole("heading", {
+        name: expectedHeadingText,
+        level: expectedHeadingLevel,
+      });
+
+      expect(expectedHeading).toBeInTheDocument();
+    });
+  });
+});
+
+describe("When it's rendered with path '/review-detail/:idReview'", () => {
+  test("Then it should show the review detail page", async () => {
+    const DetailPath = "/review-detail/:idReview";
+    const expectedHeadingText = "MovieRating";
+    const expectedHeadingLevel = 2;
+
+    renderWithProviders(
+      <MemoryRouter initialEntries={[DetailPath]}>
+        <App />
+      </MemoryRouter>
+    );
+
+    await waitFor(() => {
+      const expectedHeading = screen.queryByRole("heading", {
+        name: expectedHeadingText,
+        level: expectedHeadingLevel,
+      });
+
+      expect(expectedHeading).toBeInTheDocument();
     });
   });
 });
